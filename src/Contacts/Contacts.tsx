07ae7a6dc4a50d22f04d8fe12520784dc9d29aab
e-doschechnikova <evadrobakova@gmail.com>
2022-10-12 17:32:11 +0300

    fix component Contacts

--- src/Contacts/Contacts.tsx
+++ src/Contacts/Contacts.tsx
@@ -6,14 +6,16 @@ import {Title} from "../Components/Title/Title";
 export const Contacts = () => {
     return (
         <div className={style.contactsBlock}>
-            <div className={`${styleContainer.container} ${style.contactsContainer}`}>
-                <Title title={"Contacts"}/>
-                <form className={style.contactsForm}>
-                    <input type="text" placeholder={'Name'}/>
-                    <input type="text" placeholder={'E-mail'}/>
-                    <textarea placeholder={'Message'}/>
-                    <button type={'submit'} className={style.sendBtn}>Send message</button>
-                </form>
+            <div className={styleContainer.container}>
+                <div className={style.contactsContainer}>
+                    <Title title={"Contacts"}/>
+                    <form className={style.contactsForm}>
+                        <input type="text" placeholder={'Name'}/>
+                        <input type="text" placeholder={'E-mail'}/>
+                        <textarea placeholder={'Message'}/>
+                        <button type={'submit'} className={style.sendBtn}>Send message</button>
+                    </form>
+                </div>
             </div>
         </div>
     );
