6cfbe0cbb6b6f564423a7f32e3f9819f5c63184d
e-doschechnikova <evadrobakova@gmail.com>
2022-10-12 16:30:33 +0300

    add new block

--- src/Projects/Project/Project.tsx
+++ src/Projects/Project/Project.tsx
@@ -4,7 +4,7 @@ import style from "./Project.module.css"
 export const Project = () => {
     return (
         <div>
-            <div className={style.projectBlock}>
+            <div className={style.project}>
                 <img className={style.projectImage} src={"https://www.etudesite.ru/upload/iblock/58b/vuXO35bXcF.jpeg"} alt={'project img'}/>
                 <h4 className={style.projectName}>Project name</h4>
                 <p className={style.projectDescription}>Project description</p>
