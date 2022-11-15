77eadc3cd4210b2872389e9c90c77f8a3be68725
e-doschechnikova <evadrobakova@gmail.com>
2022-11-15 19:08:04 +0300

    fix styles,

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -21,15 +21,15 @@ export const Main = () => {
                                 <div className={style.aboutInfo}>
                                     <div className={style.row}>
                                         <div className={style.info}>
-                                            <p><b>Birthday: 15 April 1990</b></p>
-                                            <p><b>Website: www.fgorman.com</b></p>
-                                            <p><b>Phone + 99999999</b></p>
-                                            <p><b>Study: Univercity of Harvard</b></p>
+                                            <p><b>Birthday: 11 July 1996</b></p>
+                                            <p><b>Website: www.hhh.ru</b></p>
+                                            <p><b>Phone: + 999 999 99 99</b></p>
+                                            <p><b>Study: Study </b></p>
                                         </div>
                                         <div className={style.info}>
                                             <p><b>Degree: Master</b></p>
-                                            <p><b>Mail: info@fgorman.com</b></p>
-                                            <p><b>Linkedin: @francesgorman</b></p>
+                                            <p><b>Mail: info@info.com</b></p>
+                                            <p><b>Linkedin: @name</b></p>
                                             <p><b>Freelance: Available</b></p>
                                         </div>
                                     </div>
