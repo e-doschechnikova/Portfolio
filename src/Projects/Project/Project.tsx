e4435105de05dda4eb6b76093f44c531b2817641
e-doschechnikova <evadrobakova@gmail.com>
2022-11-15 10:30:31 +0300

    fix components, add styles, add sass

--- src/Projects/Project/Project.tsx
+++ src/Projects/Project/Project.tsx
@@ -9,20 +9,17 @@ type ProjectType = {
     title: string
     description: string
 }
-export const Project: FC<ProjectType> = ({styles,title, description}) => {
+export const Project: FC<ProjectType> = ({styles, title, description}) => {
     return (
-        <div>
-            <div className={style.project}>
-                <div className={style.projectImageContainer} style={styles}>
-                    <a href={"#"} className={style.projectMore}>see
-                        more</a>
-                </div>
-                <div className={style.projectInfo}>
-                    <h3 className={style.projectTitle}>{title}</h3>
-                    <div className={style.line}></div>
-                    <span className={style.projectDescription}>{description}</span>
-                </div>
-
+        <div className={style.project}>
+            <div className={style.projectImage} style={styles}>
+                <a href={"#"} className={style.projectBtn}>see
+                    more</a>
+            </div>
+            <div className={style.projectInfo}>
+                <h3 className={style.projectTitle}>{title}</h3>
+                <div className={style.line}></div>
+                <span className={style.projectDescription}>{description}</span>
             </div>
         </div>
     );
