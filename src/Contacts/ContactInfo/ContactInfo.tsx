b3cf24c761259d8ac21eba63f58417c390f3db2c
e-doschechnikova <evadrobakova@gmail.com>
2022-12-01 22:07:21 +0300

    fix styles, add new components

---
+++ src/Contacts/ContactInfo/ContactInfo.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import style from "./ContactInfo.module.scss";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {IconProp} from "@fortawesome/fontawesome-svg-core";
+
+export const ContactInfo: React.FC<ContactInfoPropsType> = ({iconTitle, contactTitle, data, link}) => {
+    return (
+        <div className={style.infoContainer}>
+            <div className={style.icon}>
+                <FontAwesomeIcon icon={iconTitle}/>
+            </div>
+            <div className={style.description}>
+                <h4>{contactTitle}</h4>
+                <a href={link}>{data}</a>
+            </div>
+        </div>
+    );
+};
+
+type ContactInfoPropsType = {
+    iconTitle: IconProp,
+    contactTitle: string,
+    data: string,
+    link: string
+}
--- src/Contacts/Contacts.tsx
+++ src/Contacts/Contacts.tsx
@@ -1,17 +1,28 @@
 import React from "react";
-import style from "./Contacts.module.css"
-import {Title} from "../Components/Title/Title";
+import style from "./Contacts.module.scss"
+import {ContactInfo} from "./ContactInfo/ContactInfo";
+import {faTelegramPlane} from "@fortawesome/free-brands-svg-icons";
+import {faEnvelope, faLocationDot} from "@fortawesome/free-solid-svg-icons";
 
 export const Contacts = () => {
     return (
         <div className={style.contactsBlock}>
             <div className={style.container}>
+                <ContactInfo iconTitle={faTelegramPlane} contactTitle={"Phone"} data={"+1 (650) 123 4444"}
+                             link={"tel:+1 (650) 123 4444"}/>
+                <ContactInfo iconTitle={faEnvelope} contactTitle={"Email"} data={"info@francesgorman.com"}
+                             link={""}/>
+                <ContactInfo iconTitle={faLocationDot} contactTitle={"Location"}
+                             data={"2 Curiosity Way, 94403, California, United States"}
+                             link={""}/>
                 <div className={style.contactsContainer}>
-                    <h2>Contacts</h2>
-                    {/*<Title title={""}/>*/}
+                    <h6 className={style.subheadingTitle}>Get in touch</h6>
+                    <h2>Contact Me</h2>
                     <form className={style.contactsForm}>
-                        <input type="text" placeholder={'Name'}/>
-                        <input type="text" placeholder={'E-mail'}/>
+                        <div className={style.inputForm}>
+                            <input type="text" placeholder={'Name'}/>
+                            <input type="text" placeholder={'E-mail'}/>
+                        </div>
                         <textarea placeholder={'Message'}/>
                         <div>
                             <button type={'submit'} className={style.sendBtn}>Send message</button>
