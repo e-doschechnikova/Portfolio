4ef507266f4e5b7998bd12a40e029e51a7007568
e-doschechnikova <evadrobakova@gmail.com>
2022-12-05 20:29:28 +0300

    fix styles

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import style from "./Footer.module.css";
+import style from "./Footer.module.scss";
 import github from "../Assets/Image/github.png"
 import linkedin from "../Assets/Image/linkedin.png"
 import telegram from "../Assets/Image/telegram.png"
@@ -10,10 +10,8 @@ export const Footer = () => {
         <div className={style.footerBlock}>
             <div className={`${styleContainer.container} ${style.footerContainer}`}>
                 <div>
-                    <p>
-                        <span>Evgeniia Doschechnikova</span>
-                    </p>
                     <div className={style.footerSocial}>
+                        <span>© 2022 All rights reserved</span>
                         <a href={"https://github.com/e-doschechnikova"}>
                             <img src={github} alt={"github"}/>
                         </a>
@@ -24,9 +22,9 @@ export const Footer = () => {
                             <img src={telegram} alt={"telegram"}/>
                         </a>
                     </div>
-                    <div className={style.bottomText}>
-                        <span>© 2022 All rights reserved</span>
-                    </div>
+                    {/*<div className={style.bottomText}>*/}
+                    {/*    <span>© 2022 All rights reserved</span>*/}
+                    {/*</div>*/}
                 </div>
             </div>
 
