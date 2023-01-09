525301009a09c9a6abb1aa2d2311b0e7bf7fd56a
e-doschechnikova <evadrobakova@gmail.com>
2023-01-09 22:47:45 +0300

    fix styles

--- src/Contacts/Contacts.tsx
+++ src/Contacts/Contacts.tsx
@@ -9,19 +9,19 @@ export const Contacts = () => {
     return (
         <div className={style.contactsBlock}>
             <div className={style.container}>
-                <ContactInfo iconTitle={faTelegramPlane} contactTitle={"Phone"} data={"+1 (650) 123 4444"}
-                             link={"tel:+1 (650) 123 4444"}/>
-                <ContactInfo iconTitle={faEnvelope} contactTitle={"Email"} data={"info@francesgorman.com"}
-                             link={""}/>
-                <ContactInfo iconTitle={faLocationDot} contactTitle={"Location"}
-                             data={"2 Curiosity Way, 94403, California, United States"}
-                             link={""}/>
+                <div>
+                    <ContactInfo iconTitle={faEnvelope} contactTitle={"Email"} data={"e.doschechnikova@gmail.com"}
+                                 link={""}/>
+                    <ContactInfo iconTitle={faLocationDot} contactTitle={"Location"}
+                                 data={"Saint - Petersburg, Russia"}
+                                 link={""}/></div>
+                <div className={style.contactsContainer}>
+                    <h6 className={style.headingTitle}>Get in touch</h6>
+                    <h2>Contact Me</h2>
+                    <ContactForm/>
+                </div>
             </div>
-            <div className={style.contactsContainer}>
-                <h6 className={style.headingTitle}>Get in touch</h6>
-                <h2>Contact Me</h2>
-                <ContactForm/>
-            </div>
+
         </div>
     )
 };
--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -8,41 +8,17 @@ export const Main = () => {
     return (
         <div className={style.mainBlock}>
             <div className={styleContainer.container}>
-                <section className={style.about}>
-                    <div className={style.container}>
-                        <div className={style.row}>
-                            <div className={style.text}>
-                                <h6 className={style.subheadingTitle}>About me</h6>
-                                <h2 className={style.headingTitle}>Evgeniia Doschechnikova</h2>
-                                <p>Hello, My name is Frances Gorman. I am a creative director and designer based in
-                                    California, USA. I love minimalism and I create stunning websites. Feel free to
-                                    contact me for any projects you might have in mind. Creative suscipit ante a
-                                    hendre. </p>
-                                <div className={style.aboutInfo}>
-                                    <div className={style.row}>
-                                        <div className={style.info}>
-                                            <p><b>Birthday: 11 July 1996</b></p>
-                                            <p><b>Website: www.hhh.ru</b></p>
-                                            <p><b>Phone: + 999 999 99 99</b></p>
-                                            <p><b>Study: Study </b></p>
-                                        </div>
-                                        <div className={style.info}>
-                                            <p><b>Degree: Master</b></p>
-                                            <p><b>Mail: info@info.com</b></p>
-                                            <p><b>Linkedin: @name</b></p>
-                                            <p><b>Freelance: Available</b></p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className={style.aboutImg}>
-                                <div className={style.img}>
-                                    <img src={mainPhoto} alt={"img"}/>
-                                </div>
-                            </div>
-                        </div>
+                <div className={style.container}>
+                    <div className={style.aboutMe}>
+                        <h6 className={style.subheadingTitle}>About me</h6>
+                        <h2 className={style.headingTitle}>Evgeniia Doschechnikova</h2>
+                        <p>HELLO, I AM FRONT-END DEVELOPER BASED IN
+                            ST. PETERSBURG</p>
                     </div>
-                </section>
+                    <div className={style.photo}>
+                        <img src={mainPhoto} alt={"img"}/>
+                    </div>
+                </div>
             </div>
         </div>
     );
